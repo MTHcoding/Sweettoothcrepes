0ffba0d9c7f3d47782d912cd10aa9c3415d3177c
MTHcoding <designerscluster@gmail.com>
2024-03-26 19:43:00 -0500

    one commit

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,8 @@ const Home = () => (
     <div className="relative flex items-center justify-center flex-col-reverse lg:flex-row">
       <div className="max-w-3xl text-center z-10">
         <h1 className="head_text">
-          Discover & Share
+          Discover 
+          <span> & Share</span>
           <br className="max-md:hidden" />
           <span className="orange_gradient"> Our Delicious Desserts</span>
         </h1>
@@ -24,8 +25,10 @@ const Home = () => (
         <Image
           src="/images/cover.png"
           alt="Art n Paper Logo"
-          width={800}
-          height={800}
+          width={500}
+          height={500}
+          quality={100}
+          loading="lazy"
           className="rounded-full"
         />
       </div>
